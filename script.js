3ce8380666d44b0a64a5a3fb1f62cc9c3d992891
pholdahl <pal.holdahl@gmail.com>
2021-09-25 21:23:29 +0200

    lots of changes

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-document.getElementById("btn1").onClick = toggleHide()
-
 function toggleHide() {
-  document.getElementById("collapsible").classList.toggle("ascii");
-  document.getElementById("bildeAvMeg").classList.toggle("hide");
+  document.getElementById("bildeAvMeg").classList.toggle("ascii");
+  document.getElementById("creators").classList.remove("sitecreators");
+  document.getElementById("creators").classList.add("hidden");
+  // document.getElementById("asciiBildeAvMeg").classList.toggle("asciiBildeAvMeg");
+}
+function toggleHide2() {
+  document.getElementById("creators").classList.toggle("sitecreators");
+  document.getElementById("bildeAvMeg").classList.remove("ascii");
+  document.getElementById("bildeAvMeg").classList.add("hidden");
+  // document.getElementById("madeOf").classList.toggle("sitecreators");
 }
