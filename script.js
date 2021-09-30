33ed463e02f851e44e3661c2f4a1ca3c55cf01b4
pholdahl <pal.holdahl@gmail.com>
2021-10-01 01:41:31 +0200

    small changes

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function toggleHide() {
-  document.getElementById("adInfoContainer").classList.toggle("showAdInfo");
+  document.getElementById("adInfoContainer").classList.add("showAdInfo");
+  document.getElementById("adInfoContainer").classList.remove("hidden");
   document.getElementById("adInfo2Container").classList.remove("showAdInfo2");
   document.getElementById("adInfo2Container").classList.add("hidden");
+  console.log("working");
 }
 function toggleHide2() {
-  document.getElementById("adInfo2Container").classList.toggle("showAdInfo2");
+  document.getElementById("adInfo2Container").classList.add("showAdInfo2");
+  document.getElementById("adInfo2Container").classList.remove("hidden");
   document.getElementById("adInfoContainer").classList.remove("showAdInfo");
   document.getElementById("adInfoContainer").classList.add("hidden");
+  console.log("working");
 }
