c0c6fb46a20d931aa8eaf5bd109e748c3fb9c88b
pholdahl <pal.holdahl@gmail.com>
2021-09-30 18:46:09 +0200

    lots of changes

--- script.js
+++ script.js
@@ -2,14 +2,36 @@ function toggleHide() {
   document.getElementById("adInfoContainer").classList.toggle("showAdInfo");
   document.getElementById("adInfo2Container").classList.remove("showAdInfo2");
   document.getElementById("adInfo2Container").classList.add("hidden");
-  // document.getElementById("asciiBildeAvMeg").classList.toggle("asciiBildeAvMeg");
 }
 function toggleHide2() {
   document.getElementById("adInfo2Container").classList.toggle("showAdInfo2");
   document.getElementById("adInfoContainer").classList.remove("showAdInfo");
   document.getElementById("adInfoContainer").classList.add("hidden");
-  // document.getElementById("madeOf").classList.toggle("sitecreators");
 }
-window.onload = function() {
-  document.getElementById("my_audio").play();
+
+function goToMusic() {
+  if (document.getElementById("musicLink").onclick) {
+    window.open("https://pholdahl.github.io/Leif_portfolio/portfolio.html","_self")
+    }
 }
+
+
+    // document.getElementById("adInfoContainer").classList.toggle("showAdInfo");
+    // document.getElementById("adInfo2Container").classList.remove("showAdInfo2");
+    // document.getElementById("adInfo2Container").classList.add("hidden");
+
+// window.onload = function() {
+//   document.getElementById("my_audio").play();
+// }
+
+// document.addEventListener("readystatechange", event => {
+//   if (event.target.readyState === "interactive") {
+
+
+  // $(document).ready(function() {
+
+    // setTimeout(function() {
+    //   500;
+    // });
+    
+    // toggleHide();
